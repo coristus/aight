658bf9128b0a018478ec712a9d70efb68cdeb890
coristus <coristus@gmail.com>
2017-06-13 13:12:01 +0200

    creates a numpy array per job

--- sortByJob.py
+++ sortByJob.py
@@ -1,16 +1,17 @@
 # Sorts data by job categorie.
 
 import numpy as np
-import sklearn as sk
 import generateData as gd
 
 (labels, data) = gd.getData()
 
+##
 # Jobs are in ISCO08_1
-
+# input labels, find job category and create set of jobs
 index = np.where(labels == 'ISCO08_1')
 jobs = set(data[:,index].flatten())
 
+# Create a numpy array per job category (ISCO08_1) and save as array
 for job in jobs:
-    select = np.where(data[:,index] == job)
-    np.save('full/job' + str(job) + '.npy', data[select])
+    select = np.where(data[:,index[0][0]] == job)
+    np.save('full/job' + str(job) + '.npy', data[select[0]])
