c2c5c7f6ca13d00b5c96dcceec7c1502a479d4c8
JelleManders <jelle4life@gmail.com>
2017-06-13 11:56:52 +0200

    added generateData.py, contains getData()

---
+++ generateData.py
@@ -0,0 +1,29 @@
+import numpy as np
+import decimal
+
+def clean(elem):
+		if elem == '' or elem == ' ':
+			elem = 0.0
+		else:
+			try:
+				elem = float(elem.replace(",","."))
+			except ValueError:
+				print "Faulty entry: " + elem
+				elem = -1.0
+		return elem
+
+def getData():
+	DATAFILE = 'full/RBFP-RDrives-CA dataset DEF.csv'
+	DELIMITER = ';'
+
+	rawData = np.genfromtxt(DATAFILE, dtype=decimal.Decimal, delimiter = DELIMITER)
+
+	(dataRows, dataCols) = rawData.shape
+
+	cleanData = np.empty((dataRows-1, dataCols), float)
+
+	for i in range(1, dataRows):
+		for j in range(0, dataCols):
+			cleanData[i-1,j] = clean(rawData[i,j])
+
+	return cleanData
