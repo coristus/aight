608406f630430bbc1f7477b6a5060031e005e36b
JelleManders <jelle4life@gmail.com>
2017-06-14 15:11:56 +0200

    added getsubdata, this only returns the data concerning drives and personalities

--- generateData.py
+++ generateData.py
@@ -2,6 +2,7 @@ import numpy as np
 import decimal
 
 CLEANDATAPATH = 'full/cleanDataFull.npy'
+CLEANSUBDATAPATH = 'full/cleanDataSub.npy'
 LABELPATH = 'full/labelsFull.npy'
 
 DATAFILE = 'full/RBFP-RDrives-CA dataset DEF.csv'
@@ -21,7 +22,7 @@ def clean(elem):
 				elem = -1.0
 		return elem
 
-##s
+##
 # Assumes fixed point decimal entries
 # getData tries to load data from meory, if this fails it
 # reparses the raw file and cleans it up.
@@ -34,6 +35,9 @@ def getData():
 		# print "One or more files not found, generating new..."
 
 		rawData = np.genfromtxt(DATAFILE, dtype=decimal.Decimal, delimiter = DELIMITER)
+
+		if sub:
+			rawData = rawData[1:,11:-1]
 
 		labels = rawData[0, :]
 		np.save(LABELPATH, labels)
@@ -49,3 +53,28 @@ def getData():
 		np.save(CLEANDATAPATH, cleanData)
 
 	return labels, cleanData
+
+##
+# same algorithm as getData, but only returns the subset of data
+# containing the drives and personality scores, saves data in
+# file CLEANSUBDATAPATH path
+def getSubData():
+	try:
+		cleanData = np.load(CLEANSUBDATAPATH)
+	except IOError:
+		# print "One or more files not found, generating new..."
+
+		rawData = np.genfromtxt(DATAFILE, dtype=decimal.Decimal, delimiter = DELIMITER)
+		rawData = rawData[1:,11:-1]
+
+		(dataRows, dataCols) = rawData.shape
+
+		cleanData = np.empty((dataRows-1, dataCols), float)
+
+		for i in range(1, dataRows):
+			for j in range(0, dataCols):
+				cleanData[i-1,j] = clean(rawData[i,j])
+
+		np.save(CLEANSUBDATAPATH, cleanData)
+
+	return cleanData
