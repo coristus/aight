b3bf94266268362d21af69cfc2f3661a32b31e51
coristus <coristus@gmail.com>
2017-06-14 16:33:23 +0200

    run isoforest with many params. count in/out occurrences and return the counts per job category. Then plot

--- outlier.py
+++ outlier.py
@@ -2,9 +2,47 @@ import numpy as np
 import sklearn as sk
 import generateData as gd
 import sortByLabel as sbl
+from sklearn.ensemble import IsolationForest as skiso
+import time
+
+
+rng = np.random.RandomState(42)
+np.random.seed(1904)
+TESTSIZE = 5
+ESTIMATOR_RANGE = range(10, 251, 20)
+MAX_SAMPLES_RANGE = range(10,1011,50)
 
 (labels, data) = gd.getData()
 
 catArrays, catLabels = sbl.splitByLabel('ISCO08_1', labels, data)
-print catLabels[6]
-print catArrays[6]
+
+outlier_predictions_per_cat = []
+
+for cat in catArrays:
+    X = cat
+    np.random.shuffle(X)
+    X_train = X[0:X.shape[0]-X.shape[0]/TESTSIZE,]
+    X_test = X[X_train.shape[0]:X.shape[0],]
+
+
+    clf = skiso(n_estimators=ESTIMATOR_RANGE[0], max_samples=MAX_SAMPLES_RANGE[0], random_state=rng, n_jobs=-1)
+    clf.fit(X_train)
+    # y_pred_train = clf.predict(X_train)
+    y_pred_test = clf.predict(X_test)
+    y_pred_test.fill(0)
+    for i in ESTIMATOR_RANGE:
+        print str(i) + 'th estimator'
+        for j in MAX_SAMPLES_RANGE:
+            clf = skiso(n_estimators=i, max_samples=j, random_state=rng, n_jobs=-1)
+            clf.fit(X_train)
+            #y_pred_train = clf.predict(X_train)
+            y_pred_test = y_pred_test+clf.predict(X_test)
+    outlier_predictions_per_cat.append(y_pred_test)
+np.save('outlier_predictions_per_cat.npy', np.array(outlier_predictions_per_cat))
+
+
+
+# for kout in K_outliers:
+#     print len(kout[0])
+
+# y_pred_outliers = clf.predict(X_outliers)
---
+++ outlierplot.py
@@ -0,0 +1,32 @@
+#plot outliers
+import matplotlib.pyplot as pl
+import numpy as np
+
+## Variables
+TESTSIZE = 5
+ESTIMATOR_RANGE = range(10, 251, 20)
+MAX_SAMPLES_RANGE = range(10,1011,50)
+MAX_SCORE = (len(ESTIMATOR_RANGE)*len(MAX_SAMPLES_RANGE))
+MIN_SCORE =-(len(ESTIMATOR_RANGE)*len(MAX_SAMPLES_RANGE))
+
+## Load predicted test outliers
+outlier_predictions_per_cat = np.load('outlier_predictions_per_cat.npy')
+
+i = 0
+for y_pred_test in outlier_predictions_per_cat:
+    K_outliers = []
+    length_outliers = []
+    for k in range(MIN_SCORE,MAX_SCORE,1):
+        outliers = np.where(y_pred_test <= k)
+        K_outliers.append(len(outliers[0])/float(y_pred_test.shape[0])*100)
+
+    pl.subplot(2,2,i+1)
+    pl.plot(range(MIN_SCORE,MAX_SCORE,1), K_outliers,'r.')
+    pl.title('Job category ' + str(i) + ' Subjects: ' + str(y_pred_test.shape[0]))
+
+    pl.xlabel('Outlier Accumulator')
+    pl.ylabel('Percentage of Outliers')
+    pl.ylim((0,60))
+    i+=1
+    if i == 4: break
+pl.show()
